ffa6318d04602bc4f6fb8b8e8c30a82b59c5a63f
CynthMartz <carol.martz21@gmail.com>
2022-10-24 13:15:57 -0300

    java arreglos

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/60/20c2253a5051001d1e6492fee6adfcfb
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author cynthia
+ *
+ */
+module GenerationC2 {
+}
---
+++ GenerationC2/src/cl/generation/v20221021/Main.java
@@ -0,0 +1,11 @@
+package cl.generation.v20221021;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Washituring!!");//simil del console.log
+
+	}
+
+}
