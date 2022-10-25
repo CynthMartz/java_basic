678a195a59844e8025b3b5326d2e7d0c11cc781e
CynthMartz <carol.martz21@gmail.com>
2022-10-25 10:53:07 -0300

    Arreglos Dinámicos

--- GenerationC2/src/cl/generation/l20221024/Arreglo.java
+++ GenerationC2/src/cl/generation/l20221024/Arreglo.java
@@ -4,41 +4,89 @@ public class Arreglo {
 
 	public static void main(String[] args) {
 
+	//int [] sinNumeros = {};
+	// System.out.println(sinNumeros.length);
 	
-				
-				int num = 56;
-		        float num2 = num;  //1
-		        System.out.println(num2);
-		        int num3 = 100;
-		        float num4 = (float) num3;//2
-		        System.out.println(num4);
-		             //////////////////
-		        float a=8.61f;
-		        int b;//3
-		        b=(int)a;
-		        System.out.println(b);
-		        /////////////////Kiara
-		        int numero1 = 20;//4
-				float num1afloat1 = numero1;
-				System.out.println(num1afloat1);
-				
-				//int p=78;
-				long num1along1 = numero1;//5
-				System.out.println(num1along1);
-				
-				double num1adouble1 = numero1;//6
-				System.out.println(num1adouble1);
-				
-				long numero2 = 234567;
-				int numero2aint1 = (int)numero2;
-				System.out.println(numero2aint1);//7
-				float num2afloat1 = numero2;
-		        /////////////////////Cynthia
-				int numm = 10;
-				double otronum = 30;
-				double result = numm*otronum;//8
-				System.out.println(result);
+	
+	//crear un array de numeros flotantes de tamaño 6
+	//float []numeros = new float[4];
+	//agregar 6 elementos al arreglo
+	/*numeros[0]= 3;
+	numeros[1]= 2;
+	numeros[2]= 7;
+	numeros[3]= 6;*/
+	
+
+	
+	//solucion 1
+			/*int[] arreInt = new int [100];
+			for (int i = 0; i < arreInt.length; i++) {
+				arreInt[i]=i;
+				System.out.println("Elemento :"+(i+1));
 			}
-		}
+			//solucion 2
+			int[] arreInt2 = new int[100];
+			for (int i = 1; i < arreInt2.length; i++) {
+				arreInt2[i]=i+1;
+			}
+			
+			System.out.println(Arrays.toString(arreInt2));*/
+			
+			//del 100 al 1
+			/*int[] arreInt2 = new int[100];
+			for (int i = 0; i < arreInt2.length; i++) {
+				arreInt2[i]=i+1;
+			}
+			System.out.println(Arrays.toString(arreInt2));
+
+			int [] ejercicio3 = new int [100];
+			for (int i= 0; i<ejercicio3.length; i++) {
+				ejercicio3[i]= 100-i;
+			}
+			System.out.println(Arrays.toString(ejercicio3));
+
+			int[] arreglo100a1 = new int[100];
+			//{100,99,98,97...0}
+			for(int i =arreglo100a1.length; i>0  ;i-- ) {
+				arreglo100a1[arreglo100a1.length - i]= i;*/
+			}
+			
+			//1,Escribir un arreglo de tamaño de 10 ,con números positivos y negativos ,
+			//capturar en máximo y mínimo
+			//2. El mismo arreglo crear otros 2 arreglos, uno con solo los pares y otro arreglo solo con los impares
+			///3. Para el mismo arreglo, sacar el promedio de los números.
+			
+			int arreglito = new int {-3,-5, 4,12, 3,35, -35,77,-1, 0};
+			
+			int [] arreglo10 = new int [10];
+			
+			int maximo = arreglito[0]; 
+			int minimo = arreglito[0]; 
+			for (int i = 0; i < 100; i++){
+				if (maximo < arreglo10[i])
+					maximo = arreglo10[i];
+				if (minimo > arreglo10[i])
+					minimo = arreglo10[i];
+			}
+
+			// Al finalizar el bucle, las variables contienen los valores máximos y mínimos.
+			System.out.println("La máximo de la lista es " + maximo);
+			System.out.println("La mínimo de la lista es " + minimo);
+			
+			int[] arreglo = {-3,-5, 4,12, 3,35, -35,77,-1, 0};
+			
+			int maxNum = arreglo[0];
+			
+			for(int i : arreglo) {
+				if(i > maxNum)
+					maxNum = i;
+			}
+			System.out.println(maxNum);
+	}	
+			
+			
+}
+			
+	
 
 	
---
+++ GenerationC2/src/cl/generation/l20221024/ejerciciosito.java
@@ -0,0 +1,42 @@
+package cl.generation.l20221024;
+
+public class ejerciciosito {
+
+	public static void main(String[] args) {
+
+		////1,Escribir un arreglo de tamaño de 10 ,con números positivos y negativos ,
+		//capturar en máximo y mínimo
+		
+		
+		int[] arreglo = {-3,-5, 4,12, 3,35, -35,77,-1, 0};
+		
+		int maxNum = arreglo[0];
+		
+		for(int i : arreglo) {
+			if(i > maxNum)
+				maxNum = i;
+		}
+		int minNum = arreglo[0];
+		for(int i : arreglo) {
+			if(i < minNum)
+				minNum = i;
+		}
+		System.out.println(maxNum);
+		System.out.println(minNum);
+		
+		
+		//2. El mismo arreglo crear otros 2 arreglos, uno con solo los pares y otro arreglo solo con los impares
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				///3. Para el mismo arreglo, sacar el promedio de los números.
+
+	}
+
+}
